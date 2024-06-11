2b001f738e0ceac5f73012a285f5a0ae9f77f5cf
Rajiv Ravindra pathak <70104100+rajivravindrapathak@users.noreply.github.com>
2024-06-11 10:49:57 +0530

    Add files via upload

--- server/index.js
+++ server/index.js
@@ -1,1 +1,27 @@
+const express = require('express')
+const cors = require('cors');
+const { connection } = require('./config/db');
+const bookRoutes = require('./routes/books')
+
+const app = express()
+
+require('dotenv').config()
+app.use(cors());
+// app.use(bodyParser.json());
+
+// const app = require('./app');
+const port = process.env.PORT || 5000;
+
+app.use('/api/books', bookRoutes);
+
+app.listen(port, async() => {    
+  try {
+    await connection
+    console.log('connected to db')
+  } catch (error) {
+    console.log(error)
+    console.log('not connected to db')
+  }
+  console.log(`Server is running on port ${port}`);
+});
 
