311e1b708dbcd6521ce7c22b480476cada0eea33
Rajiv Ravindra pathak <70104100+rajivravindrapathak@users.noreply.github.com>
2024-06-11 10:48:37 +0530

    Add files via upload

---
+++ server/app.js
@@ -0,0 +1,19 @@
+// const express = require('express');
+// const mongoose = require('mongoose');
+// const cors = require('cors');
+// const bodyParser = require('body-parser');
+
+// const app = express();
+
+// mongoose.connect('mongodb://localhost:27017/bookreviewapp', {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+// });
+
+// app.use(cors());
+// app.use(bodyParser.json());
+
+// const bookRoutes = require('./routes/books');
+// app.use('/api/books', bookRoutes);
+
+// module.exports = app;
